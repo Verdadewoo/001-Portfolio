61f163bbc9d999cccc28725be0b7f57e8628e0a7
verdadewoo <jeffersonverdadero369@gmail.com>
2023-06-23 17:34:17 +0800

    Better Landing

--- js/index.js
+++ js/index.js
@@ -1,51 +1,12 @@
-const nameElement = document.getElementById('name');
-    const names = ['a Developer', 'an Artist','an Editor', 'Jefferson'];
-    let currentIndex = 0;
-    let isTyping = true;
-    let currentText = '';
-    let currentIndexLetter = 0;
-    let delay;
+AOS.init();
 
-    function typeText() {
-      if (isTyping) {
-        currentText = names[currentIndex].substring(0, currentIndexLetter);
-        nameElement.textContent = currentText;
-        currentIndexLetter++;
+var typed3 = new Typed('#typed3', {
+  strings: ['a Developer', 'an Artist','an Editor', 'Jefferson'],
+  typeSpeed: 80,
+  backSpeed: 50,
+  smartBackspace: true, // this is a default
+  loop: true
+});
 
-        if (currentIndexLetter <= names[currentIndex].length) {
-          setTimeout(typeText, 100);
-        } else {
-          delay = setTimeout(deleteText, 2000);
-        }
-      } else {
-        currentText = names[currentIndex].substring(0, currentIndexLetter);
-        nameElement.textContent = currentText;
-        currentIndexLetter--;
-
-        if (currentIndexLetter < 0) {
-          isTyping = true;
-          currentIndex = (currentIndex + 1) % names.length;
-          setTimeout(typeText, 0); // Display next word instantly
-        } else {
-          setTimeout(deleteText, 100);
-        }
-      }
-    }
-
-    function deleteText() {
-      isTyping = false;
-      currentText = names[currentIndex].substring(0, currentIndexLetter);
-      nameElement.textContent = currentText;
-      currentIndexLetter--;
-
-      if (currentIndexLetter < 0) {
-        clearTimeout(delay); // Clear the delay after deletion
-        isTyping = true;
-        currentIndex = (currentIndex + 1) % names.length;
-        setTimeout(typeText, 0); // Display next word instantly
-      } else {
-        setTimeout(deleteText, 50);
-      }
-    }
-
-    setTimeout(typeText, 2000);
+$('.my-element').paroller();
+$('.jumbotron').paroller();
